2341a3231a0e48547ddc16c982dc9b4d0eff7730
Ilya Miflig <ilya_miflig@epam.com>
2023-11-26 01:16:38 +0200

    NodeJS - minor update

--- Aquarium/NodeJS_tests/common/action/common_actions.js
+++ Aquarium/NodeJS_tests/common/action/common_actions.js
@@ -26,11 +26,11 @@ exports.switchToDevice = async function(deviceConfig) {
 
 exports.doFeed = async function() {
   let vars = {};
-  vars["feedState"] = await driver.findElement(By.xpath("//div[@id=\'WEB_SWITCH1\']//span[contains(@class, \'label\')]")).getText()
+  vars["feedState"] = await driver.findElement(By.xpath("//div[@id='WEB_SWITCH1']//span[contains(@class, 'label')]")).getText()
   console.log("feedState = " + vars["feedState"]);
-  if (!!await driver.executeScript("return (arguments[0]==\"Done\")", vars["feedState"])) {
-    await driver.findElement(By.xpath("//div[@id=\'WEB_SWITCH1\']//button")).click()
+  if (!!await driver.executeScript("return (arguments[0]=='Done')", vars["feedState"])) {
+    await driver.findElement(By.xpath("//div[@id='WEB_SWITCH1']//button")).click()
     await driver.sleep(1000)
   }
-  await driver.findElement(By.xpath("//div[@id=\'WEB_SWITCH1\']//button")).click()
+  await driver.findElement(By.xpath("//div[@id='WEB_SWITCH1']//button")).click()
 }
--- Aquarium/NodeJS_tests/test_v1.js
+++ Aquarium/NodeJS_tests/test_v1.js
@@ -1,21 +1,10 @@
-var commonActions = require('./common/action/common_actions.js');
-var devices = require('./common/config/devices.js');
-
-require("chromedriver");
-
-const { Builder, By, Key } = require("selenium-webdriver");
-var chrome = require("selenium-webdriver/chrome");
-const path = require("chromedriver").path;
-
-let vars = {};
-
-
+var commonActions = require("./common/action/common_actions.js");
+var devices = require("./common/config/devices.js");
 
 //describe - describes test
 describe("Open device", function () {
   //it - describes expected behaviour
   it("should open device", async function () {
-    // try {
     await driver.get("https://blynk.cloud/dashboard/login");
     await commonActions.login();
     await commonActions.switchToDevice(devices.getAquariumTestConfig());
@@ -23,8 +12,5 @@ describe("Open device", function () {
     await commonActions.doFeed();
 
     console.log("TEST PASSED");
-    // } finally {
-    // await driver.quit();
-    // }
   }).timeout(100000);
 });
