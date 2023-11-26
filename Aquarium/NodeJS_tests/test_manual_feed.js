12bed6fdfa19124932d2ac9da5429a8d8623a547
Helen-Sen <senchikhina.elena@gmail.com>
2023-11-26 18:34:38 +0200

    add asserts

--- Aquarium/NodeJS_tests/test_manual_feed.js
+++ Aquarium/NodeJS_tests/test_manual_feed.js
@@ -1,3 +1,6 @@
+var chai = require("chai");
+var assert = chai.assert;
+
 require("./common/main_objects.js");
 var commonActions = require("./common/action/common_actions.js");
 var aquarium = require("./common/action/aquarium_actions.js");
@@ -7,27 +10,36 @@ var devices = require("./common/config/devices.js");
 describe("Aquarium-Test - manual feed", function () {
   //it - describes expected behaviour
   it("Aquarium-Test should do manual feed", async function () {
-    await driver.get("https://blynk.cloud/dashboard/login");
-    await commonActions.login();
-    await commonActions.switchToDevice(devices.getAquariumTestConfig());
-    await driver.sleep(1000);
-    await aquarium.doFeed();
-    await driver.sleep(15000);
-    var lastfeed = await aquarium.getLastFeedTime();
-    // await driver.sleep(1000);
-    console.log(lastfeed["lastFeedDateTime"]);
-    console.log(lastfeed["lastFeedHours"]);
-    console.log(lastfeed["lastFeedMinutes"]);
+    try {
+      await driver.get("https://blynk.cloud/dashboard/login");
+      await commonActions.login();
+      await commonActions.switchToDevice(devices.getAquariumTestConfig());
+      await driver.sleep(1000);
+      await aquarium.doFeed();
+      await driver.sleep(10000);
+      var lastfeed = await aquarium.getLastFeedTime();
+      // await driver.sleep(1000);
+      var lastFeedHours = lastfeed["lastFeedHours"];
+      var lastFeedMinutes = lastfeed["lastFeedMinutes"];
+      console.log(lastfeed["lastFeedDateTime"]);
+      console.log(lastFeedHours);
+      console.log(lastFeedMinutes);
 
-    var systemTime = await commonActions.getSystemTime();
-    var systemHours = systemTime["systemHours"];
-    var systemMinutes = systemTime["systemMinutes"];
-    console.log(systemTime["systemTime"]);
-    console.log(systemHours);
-    console.log(systemMinutes);
-    
+      var systemTime = await commonActions.getSystemTime();
+      var systemHours = systemTime["systemHours"];
+      var systemMinutes = systemTime["systemMinutes"];
+      console.log(systemTime["systemTime"]);
+      console.log(systemHours);
+      console.log(systemMinutes);
 
-    console.log("TEST PASSED");
+      assert.equal(lastFeedHours, systemHours, "Feed hours not match with system hours");
+      assert.equal(lastFeedMinutes, systemMinutes, "Feed minutes not match with system minutes");
+
+      console.log("TEST PASSED");
+    } finally {
+      await driver.sleep(1000);
+      await driver.quit();
+    }
   }).timeout(100000);
 
   // it("Aquarium-Test should do manual feed 2", async function () {
